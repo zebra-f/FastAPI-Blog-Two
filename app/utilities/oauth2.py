3f79a03d5edadb4c2c832aaa0958ba8b747f438c
zebra-f <rafal.zebrowski@gmail.com>
2022-01-21 18:30:16 +0100

    securing post endpoints from non authorized users

--- app/utilities/oauth2.py
+++ app/utilities/oauth2.py
@@ -1,17 +1,18 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Depends
+from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordBearer
 from datetime import timedelta, datetime
 from typing import Optional
 from jose import JWTError, jwt
 
-from .. import schemas
+from .. import schemas, database, models
 
 # used by get_curret_user
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 45
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -33,7 +34,7 @@ def verify_access_token(token, credentials_exception):
     '''
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
+        id: int = payload.get("user_id")
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
@@ -43,12 +44,15 @@ def verify_access_token(token, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-    return verify_access_token(token, credentials_exception)
+    token_data = verify_access_token(token, credentials_exception)
+    current_user = db.query(models.User).filter(models.User.id == token_data.id).first()
+
+    return current_user
 
