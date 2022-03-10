2c1f6ce8baf1496884648ac49639b2238240cc3a
zebra-f <rafal.zebrowski@gmail.com>
2022-03-11 00:46:37 +0100

    fixed pytest-checkup branch, test_calculations, calculations

--- app/calculations.py
+++ app/calculations.py
@@ -1,5 +1,18 @@
 # PyTest checkup
 
+
 def add(num1: int, num2: int=2) -> int:
     return num1 + num2
+
+
+def subtract(num1: int, num2: int=2) -> int:
+    return num1 - num2
+
+
+def multiply(num1: int, num2: int=2) -> int:
+    return num1 * num2
+
+
+def divide(num1: int, num2: int=2) -> int:
+    return num1 / num2
 
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,9 +1,21 @@
-from app.calculations import add
+from app.calculations import add, subtract, multiply, divide
 
 
 def test_add():
-    print("testing function")
+    print("testing addition")
     assert add(3, 4) == 7
+    assert add(1) == 3
 
-test_add()
 
+def test_subtract():
+    print("testing subtraction")
+    assert subtract(3, 4) == -1
+    assert subtract(1) == -1
+
+
+def test_calculations():
+    assert multiply(3, 4) == 12
+    assert multiply(1) == 2
+
+    assert divide(3, 4) == 3/4
+    assert divide(1) == 0.5
