3d993570dce736d32e3121ee687a81da1b12cf2a
zebra-f <rafal.zebrowski@gmail.com>
2022-03-11 00:12:09 +0100

    rm calc, test_calc

--- app/calculations.py
+++
@@ -1,5 +0,0 @@
-# PyTest checkup
-
-def add(num1: int, num2: int=2) -> int:
-    return num1 + num2
-
--- tests/test_calculations.py
+++
@@ -1,9 +0,0 @@
-from app.calculations import add
-
-
-def test_add():
-    print("testing function")
-    assert add(3, 4) == 7
-
-test_add()
-
