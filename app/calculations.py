4e9db1b0dc2166248069a7986c660ce89d4a7635
zebra-f <rafal.zebrowski@gmail.com>
2022-03-11 18:43:43 +0100

    finished checkup

--- app/calculations.py
+++ app/calculations.py
@@ -16,3 +16,24 @@ def multiply(num1: int, num2: int=2) -> int:
 def divide(num1: int, num2: int=2) -> int:
     return num1 / num2
 
+
+class InsufficientFundsException(Exception):
+    pass
+
+
+class BankAccount:
+    
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientFundsException("Insufficient funds!")
+        
+        self.balance -= amount
+
+
+    def deposit(self, amount):
+        self.balance += amount
+
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,17 @@
 import pytest
 
-from app.calculations import add, subtract, multiply, divide
+from app.calculations import (add, subtract, multiply, divide, BankAccount,
+                             InsufficientFundsException)
+
+
+@pytest.fixture
+def default_bank_account_fixture():
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account_fixture():
+    return BankAccount(100)   
 
 
 @pytest.mark.parametrize("num1, num2, expected", [
@@ -29,3 +40,60 @@ def test_calculations():
 
     assert divide(3, 4) == 3/4
     assert divide(1) == 0.5
+
+
+def test_bank_account_one():
+    
+    customer_instance_one = BankAccount()
+    assert customer_instance_one.balance == 0
+
+    customer_instance_one.deposit(40)
+    assert customer_instance_one.balance == 40
+
+    customer_instance_one.withdraw(10)
+    assert customer_instance_one.balance == 30
+
+
+def test_bank_account_two():
+    
+    custom_instance_two = BankAccount(100)
+    assert custom_instance_two.balance == 100
+
+    custom_instance_two.deposit(40)
+    assert custom_instance_two.balance == 140
+
+    custom_instance_two.withdraw(10)
+    assert custom_instance_two.balance == 130
+
+
+def test_bank_defualt_balance(default_bank_account_fixture):
+    assert default_bank_account_fixture.balance == 0
+
+
+def test_bank_balance(bank_account_fixture):
+    assert bank_account_fixture.balance == 100
+
+
+def test_bank_withdraw(bank_account_fixture):
+    bank_account_fixture.withdraw(10)
+    assert bank_account_fixture.balance == 90
+
+
+def test_bank_deposit(bank_account_fixture):
+    bank_account_fixture.deposit(40)
+    assert bank_account_fixture.balance == 140
+
+
+@pytest.mark.parametrize("d, w, b", [
+    (20, 10, 10),
+    (1700, 0, 1700),
+])
+def test_bank_default(default_bank_account_fixture, d, w, b):
+    default_bank_account_fixture.deposit(d)
+    default_bank_account_fixture.withdraw(w)
+    assert default_bank_account_fixture.balance == b
+
+
+def test_bank_default_insufficient_funds(default_bank_account_fixture):
+    with pytest.raises(InsufficientFundsException):
+        default_bank_account_fixture.withdraw(40)
