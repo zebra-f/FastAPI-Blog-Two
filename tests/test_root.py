039c2dd60b5030fbb6a0bc0015028dc02f15a3e5
zebra-f <rafal.zebrowski@gmail.com>
2022-03-14 16:01:11 +0100

    added test_create_user test_users.py, minor change to test_root.py

--- tests/test_root.py
+++ tests/test_root.py
@@ -3,8 +3,11 @@ from app.main import client
 
 def test_root():
     res = client.get("/")
+
+    assert res.status_code == 200
+
     assert res.json().get("Documentation") == {
                 'Swagger UI': 'http://127.0.0.1:8000/docs', 
                 'ReDoc': 'http://127.0.0.1:8000/redoc'
                 }
-    assert res.status_code == 200
+    
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,20 @@
 from app.main import client
+from app.utilities import b_pass
+from app import schemas
 
+
+def test_create_user():
+    email = "tests_user4@email.com"
+    password = b_pass.get_password_hash("tests_password")
+    
+    response = client.post("/users/", json={
+        "email": email,
+        "password": password
+    })
+
+    assert response.status_code == 201
+
+    new_user = schemas.UserResponse(**response.json())
+    assert new_user.email == email
 
     
